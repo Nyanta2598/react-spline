88a0c913b8f44893f1153fc32e2b8c9cbf8cb66f
Prince Joseph Esteves <74764886+Nyanta2598@users.noreply.github.com>
2022-11-16 10:11:51 +0800

    mojo

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import Spline from '@splinetool/react-spline';
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/NlPdRmtReA7rKlQS/scene.splinecode" />
+    <div class="container mx-auto px-4">
+      <Spline scene="https://prod.spline.design/NlPdRmtReA7rKlQS/scene.splinecode" />
+    </div>
   );
 }
---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding: '2rem',
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
