fc8bf21581f1c2fc15b4ecd3a3d700e0d51b3457
Prince Joseph Esteves <74764886+Nyanta2598@users.noreply.github.com>
2022-11-18 12:54:17 +0800

    heyhey

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
                 <span></span>
    
                 <ul id="menu">
-                    <a><li>Welcome to Molang's world!</li></a>
+                    <li><h1>Welcome to Molang's world!</h1></li>
                     <li><p className='para'>Molang is a French-South Korean animated children's television series created by the animation 
                         studio Millimages. The titular character, Molang, was designed by the Korean illustrator Hye-Ji Yoon on the platform KakaoTalk.</p></li>
                     <li><p className='para'>The series first aired on Canal+ France in November 2, 2015. Since then, now 5 seasons, 9 TV specials 
                         and a music album have been produced and are widely broadcast.</p></li>
-                    <a><li>Made By:</li></a>
+                    <li><h1>Made By:</h1></li>
                     <li><p className='head'>Prince Joseph Esteves</p></li>
                 </ul>
             </div>
